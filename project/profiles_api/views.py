8b73c24016a1a6a0c06756f6be5aef1e6b854f14
Svilen Dyakovski <sdyakovski@gmail.com>
2022-04-20 23:31:38 -0700

    Added a Test Hello API View, and configured it in urls.py. Tested in several ways.

--- project/profiles_api/views.py
+++ project/profiles_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test the API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of API View features"""
+
+        an_apiview = [
+            'Uses HTTP methods as its methods (get, post, put, patch, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs'
+        ]
+        # must return a Response object.
+        # The Response should contain a list or a dictionary.
+        # It converts it to JSON
+        return Response({
+            'message': 'Hello!',
+            'an_apiview': an_apiview
+        })
--- project/profiles_project/urls.py
+++ project/profiles_project/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from profiles_api.views import HelloAPIView
 
+# adding my hello view test in 2 ways:
+# - as a Class-based view directly
+# - including the profiles_api app's urls configuration.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', HelloAPIView.as_view()),
+    path('api/', include('profiles_api.urls'))
 ]
